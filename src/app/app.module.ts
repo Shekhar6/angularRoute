344820360c5d8c5080e25ca4a1f5447f53bed840
Shekhar6 <polepakashekar100@gmail.com>
2021-10-25 16:41:29 +0530

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import { EmployeeModule } from './employee/employee.module';
+import { DepartmentModule } from './department/department.module';
+import { AppRoutingModule } from './app.routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    EmployeeModule,
+    DepartmentModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/department/department.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DepartmentComponent } from './department.component';
+
+
+
+@NgModule({
+  declarations: [
+    DepartmentComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [DepartmentComponent]
+})
+export class DepartmentModule { }
